fd92c5ad6950502d3c6cbdd58e76261f0c4d5985
Matthew Moskwa <matt@pubget.com>
2014-06-05 19:39:41 -0400

    added class helper and recipe.has_ingredients
    QueryHelper class can be includedin ndb.Model subclasses to add generic from_field queries
    recipe.has_ingredients(ingredients) method tests for inclusion of ingredients (some or all) in recipe (not working)

--- drink-o-matic/main.py
+++ drink-o-matic/main.py
@@ -22,48 +22,81 @@ import collections
 import itertools
 from google.appengine.ext import ndb
 
-class Recipe(ndb.Model):
+class QueryHelper(object):
+    @classmethod
+    def from_name(cls, name):
+        return cls.from_key("name", name)
+
+    @classmethod
+    def from_key(cls, key, value):
+        try:
+            return cls.query(getattr(cls, key)==value).fetch(1)[0]
+        except IndexError:
+            print "no value at "+value
+            return None
+
+
+class Recipe(ndb.Model, QueryHelper):
     name = ndb.StringProperty(required=True)
-    
+
     def ingredients(self):
-        return [(ingr.ingr.get(), ingr.amt) for 
+        return [(ingr.ingr.get(), ingr.amt) for
             ingr in RecipeIngredient.query(RecipeIngredient.recipe==self.key).fetch()]
 
+    def has_ingredients(self, ingredients, only=False):
+        """docstring for has_ingredients"""
+        ingr = self.ingredients()
+        print ingr
+        print ingredients
+        if only and set(ingr) == set(ingredients):
+            return True
+        else:
+            return all(x in ingr for x in ingredients)
 
-class Ingredient(ndb.Model):
+class Ingredient(ndb.Model, QueryHelper):
     name = ndb.StringProperty(required=True)
     ingr_type = ndb.StringProperty(required=True, choices=["alcohol", "mixer"])
-    
+
     def recipes(self):
-        return [ recipe.get() for recipe in RecipeIngredient.query(RecipeIngredient.ingr==self.key).fetch()]
+        return [ r_i.recipe.get() for r_i in RecipeIngredient.query(RecipeIngredient.ingr==self.key).fetch() ]
 
 class IngredientsList(object):
     def __init__(self, ingr_list):
         """docstring for __init__"""
         self.ingr_list = ingr_list
-    
+
     def ingr_combinations(self):
         combinations = []
         for element in range(1, len(self.ingr_list)+1):
             combinations.append(itertools.combinations(self.ingr_list, element))
-        return list(itertools.chain.from_iterable(combinations))     
-    
+        return list(itertools.chain.from_iterable(combinations))
+
     def all_recipes(self):
         combinations = self.ingr_combinations()
+        all_recipes = []
         for combo in combinations:
-            # get all recipes for the ingredients
-            recipes = [ ingr.recipes() for ingr in combo ]   
-            # then 
+            # get all recipes for the ingredients and flatten
+            _recipes = []
+            for ingr in combo:
+
+                r = Ingredient.from_name(ingr).recipes()
+                _recipes.append(r)
+
+            #_recipes = [ recipe for ingr in combo for recipe in Ingredient.from_name(ingr).recipes() ]
+            _recipes = [ r for rec in _recipes for r in rec ]
+            print _recipes
+        # then remove any recipes that don't have all ingredients in the combo
+            recipes = [ rec for rec in _recipes if rec.has_ingredients(combo) ]
+        all_recipes += recipes
+
+        return all_recipes
+
 class RecipeIngredient(ndb.Model):
     recipe = ndb.KeyProperty(kind=Recipe, required=True)
     ingr = ndb.KeyProperty(kind=Ingredient, required=True)
     amt = ndb.FloatProperty(required=True)
 
 class MainHandler(webapp2.RequestHandler):
-    # Return all combinations of list *ingr* as a list of tuples
-    def ingr_combinations(self, ingr):
-
-        
     def get(self):
         self.response.write('Hello world! <br>')
 
@@ -72,7 +105,8 @@ class MainHandler(webapp2.RequestHandler):
 
         # get all the recipes that correspond to these ingredients (some or all)
         # first generate all the ingredient combinations
-        combos = self.ingr_combinations
+        ingr_list = IngredientsList(my_ingr)
+        combos = ingr_list.ingr_combinations
         ingredients = Ingredient.query(Ingredient.name.IN(my_ingr)).fetch(3)
 
         # for each subset, get the recipes that correspond
