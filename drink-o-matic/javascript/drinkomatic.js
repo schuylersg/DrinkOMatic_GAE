1cb981e1e26b68fb859c9d8716d4fcf673e75f41
Schuyler Senft-Grupp <schuylersg@gmail.com>
2014-06-08 21:34:56 -0400

    All DIV sizes rendering properly

--- drink-o-matic/javascript/drinkomatic.js
+++ drink-o-matic/javascript/drinkomatic.js
@@ -1,11 +1,25 @@
-
-
 $(document).ready(function() {
-	var height = $(window).height() - 75; 
-	$("#main-div").height(height);
+	var totHeight = $(window).height();
+	var totWidth = $(window).width();
+	var titleHeight = $('#title-image').outerHeight(true);
+	var optionsHeight = $('#options-bar').outerHeight(true);
+	$("#main-div").height(totHeight-18);
+	$("#main-div").width(totWidth-8);	
+	
+	drawableHeight = $("#main-div").innerHeight() - titleHeight - optionsHeight;
+	
+	$("#canvas-wrap").height(drawableHeight);
+	$("#canvas-wrap").width($("#main-div").innerWidth());
+	$("canvas").height(drawableHeight);
+	
+	$("#selected-liquors").height(drawableHeight);
+	$("#selected-liquors").width(100);
+	
+	$("#selected-mixers").height(drawableHeight);
+	$("#selected-mixers").width(100);
 
-	$("#canvas-wrap").height(height-100);
-	$("canvas").height(height-100);
+	$("#cocktails-div").height(drawableHeight);
+	$("#cocktails-div").width(totWidth-8-200);
 	
 	$(".menu-item").click(function() {
 		$( this ).toggleClass("menu-item-select ");
@@ -16,13 +30,36 @@ $(document).ready(function() {
 		}
 		$( "#overlay" ).empty();
 		$( "#overlay" ).load( "/UpdateIngr", dataToSend, function() {
-			;
+
+			var totHeight = $(window).height();
+			var totWidth = $(window).width();
+			var titleHeight = $('#title-image').outerHeight(true);
+			var optionsHeight = $('#options-bar').outerHeight(true);
+			$("#main-div").height(totHeight-18);
+			$("#main-div").width(totWidth-8);	
+			
+			drawableHeight = $("#main-div").innerHeight() - titleHeight - optionsHeight;
+			
+			$("#canvas-wrap").height(drawableHeight);
+			$("#canvas-wrap").width($("#main-div").innerWidth());
+			$("canvas").height(drawableHeight);
+			
+			$("#selected-liquors").height(drawableHeight);
+			$("#selected-liquors").width(100);
+			
+			$("#selected-mixers").height(drawableHeight);
+			$("#selected-mixers").width(100);
+
+			$("#cocktails-div").height(drawableHeight);
+			$("#cocktails-div").width(totWidth-8-200);	
+		
 		});
 	});
 	
 	$("#change-view").click(function() {
 		$("#canvas-wrap").toggleClass("show hide");
 		$("#ing-menu-div").toggleClass("show hide");
+		($(this).text() === "Menu") ? $(this).text("Ingredients") : $(this).text("Menu");		
 	});
 	
 });
